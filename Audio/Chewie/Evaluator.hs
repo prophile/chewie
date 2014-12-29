36b84faa38a25b93e292c63081153882dc4fffd9
Alistair Lynn <arplynn@gmail.com>
2014-12-29 02:19:29 +0000

    Pass in the sampling rate for the evaluator

--- Audio/Chewie/Evaluator.hs
+++ Audio/Chewie/Evaluator.hs
@@ -25,18 +25,22 @@ getSample (Trapeze x _ _) = x
 trapezeIntegrate :: Fractional n => Time -> Time -> Time -> (Time -> n) -> n
 trapezeIntegrate dt t0 t1 f = total samples
   where samples = collectTrapezeSamples t0
-        collectTrapezeSamples t | t >= t1 = TrapezeEnd (f t1)
+        collectTrapezeSamples t | t > t1 = error "sampling beyond end"
+        collectTrapezeSamples t | t == t1 = TrapezeEnd (f t1)
+        collectTrapezeSamples t | t < t0 = error "sampling before beginning"
         collectTrapezeSamples t | otherwise = let nextPoint = min (t + dt) t1
                                                   delta = nextPoint - t
                                                   sample = f t
                                                   rest = collectTrapezeSamples nextPoint
-                                                    in Trapeze sample delta rest
+                                                    in if delta <= 0 then error "dt <= 0"
+                                                                     else Trapeze sample delta rest
         total (Trapeze x d y) = total y + (fromRational (d / 2))*(x + getSample y)
         total (TrapezeEnd _) = 0
 
 blatantLiesConvolution :: Fractional n => (Time -> n) -> (Time -> n) -> Time -> n
 blatantLiesConvolution f s = let k = s 0 in \t -> k * f t
 
-defaultEvaluator = Evaluator { evalIntegrate = trapezeIntegrate (1 % 44100),
-                               evalConvolve  = blatantLiesConvolution }
+defaultEvaluator :: Integer -> Evaluator
+defaultEvaluator hz = Evaluator { evalIntegrate = trapezeIntegrate (1 % hz),
+                                  evalConvolve  = blatantLiesConvolution }
 
